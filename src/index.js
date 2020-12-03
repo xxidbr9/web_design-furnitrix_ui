f9bf857430d86bf04241f67f3fa12ec7a70754f6
Barnando Akbarto <barnando13@gmail.com>
2020-12-03 23:58:42 +0700

    fix croco build error

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import "assets/css/index.css";
+import "assets/css/tailwind.css";
 // import "assets/scss/base.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
